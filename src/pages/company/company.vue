99279891502b0df801c1f40ee0cfa6d6b151f7bb
YNB123 <1759997615@qq.com>
2019-02-28 17:43:10 +0800

    首页head开发

---
+++ src/pages/company/company.vue
@@ -0,0 +1,19 @@
+<template>
+<div>
+            <router-link to="/home">COMPANY !</router-link>
+</div>
+</template>
+
+
+<script>
+export default {
+ name:"company"
+}
+</script>
+
+<style>
+
+</style>
+
+
+
---
+++ src/pages/home/components/Header.vue
@@ -0,0 +1,63 @@
+<template>
+<div class="header">
+        
+        <div class="header-left">
+            <div class="iconfont back-icon">&#xe624;</div>
+        </div>
+        <div class="header-input">
+             <span class="iconfont">&#xe632;</span>
+             输入城市/景点/游玩主题</div>
+        <div class="header-right">城市
+         <span class="iconfont area-icon">&#xe64a;</span>
+
+
+
+
+        </div>
+
+</div>
+</template>
+
+
+<script>
+export default {
+ name:"HomeHeader"
+}
+</script>
+<!-- scoped 限制样式的作用域，只对当前组件有效，不会影响到其他组件 -->
+ 
+<style lang="stylus" scoped>
+@import '~sty/varibles.styl';
+.header
+ 
+    display flex
+    line-height:.86rem
+    background:$bgcolor
+    color:#fff
+    .header-left 
+     width :.64rem;
+     float :left 
+      .back-icon
+       text-align:center
+       font-size:.4rem
+    .header-input
+     flex:1
+     height:.64rem
+     line-height:.64rem
+     margin-top:.11rem
+     margin-left: .2rem
+     background:#fff
+     color:#ccc
+     border-radius:.1rem
+    .header-right
+     width 1.24rem;
+     text-align:center
+     float:right 
+      .area-icon
+       font-size :.24rem
+      margin-left : -.04rem
+
+</style>
+
+
+
